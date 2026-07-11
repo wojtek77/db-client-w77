e0958fbc3866d62be265aca9115d5f45f3e0b52e
Wojciech Brüggemann <wojbru77@gmail.com>
2026-07-11 06:53:21 +0200

    Revert "improve start and stop extension"
    This reverts commit f5c9edc773035952c161730263a8fc1bd51d5b79.

--- src/lifecycle/extensionLifecycle.ts
+++ src/lifecycle/extensionLifecycle.ts
@@ -4,39 +4,19 @@ import { RecentSqlFiles } from '../recentFiles/RecentSqlFiles.js';
 import { TableColumnsCache } from '../cache/TableColumnsCache.js';
 
 let extensionRunning = false;
-let startingPromise: Promise<void> | null = null;
-let stoppingAllPromise: Promise<void> | null = null;
 
 export function isExtensionRunning() {
     return extensionRunning;
 }
 
 export async function startExtension(context: vscode.ExtensionContext) {
-    // już uruchomione - nic do zrobienia
-    if (extensionRunning) {
-        return;
-    }
-
-    // start już w toku (druga równoległa próba) - poczekaj na ten sam start
-    if (startingPromise) {
-        return startingPromise;
-    }
-
-    startingPromise = (async () => {
-        console.log('START_EXTENSION');
-
-        // ⭐ USTAW KONTEKST – zakładka stanie się widoczna
-        await vscode.commands.executeCommand('setContext', 'dbClientActive', true);
-        extensionRunning = true;
-
-        ConnectionManager.getInstance().start();
-    })();
-
-    try {
-        await startingPromise;
-    } finally {
-        startingPromise = null;
-    }
+    console.log('START_EXTENSION');
+    
+    // ⭐ USTAW KONTEKST – zakładka stanie się widoczna
+    await vscode.commands.executeCommand('setContext', 'dbClientActive', true);
+    extensionRunning = true;
+    
+    ConnectionManager.getInstance().start();
 }
 
 export async function stopExtension(all = false) {
@@ -52,24 +32,11 @@ export async function stopExtension(all = false) {
     TableColumnsCache.getInstance().clearTableColumnsCache();
     
     if (all) {
-        // stop-all już w toku (druga równoległa próba) - poczekaj na ten sam stop
-        if (stoppingAllPromise) {
-            return stoppingAllPromise;
-        }
-
-        stoppingAllPromise = (async () => {
-            // zamknięcie panelu na dole
-            await vscode.commands.executeCommand('workbench.action.closePanel');
-
-            // ⭐ UKRYJ ZAKŁADKĘ
-            await vscode.commands.executeCommand('setContext', 'dbClientActive', false);
-            extensionRunning = false;
-        })();
-
-        try {
-            await stoppingAllPromise;
-        } finally {
-            stoppingAllPromise = null;
-        }
+        // zamknięcie panelu na dole
+        await vscode.commands.executeCommand('workbench.action.closePanel');
+        
+        // ⭐ UKRYJ ZAKŁADKĘ
+        await vscode.commands.executeCommand('setContext', 'dbClientActive', false);
+        extensionRunning = false;
     }
 }
